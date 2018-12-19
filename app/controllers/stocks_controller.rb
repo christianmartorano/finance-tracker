bf552de9a6e04de58f8f7e30e1be8f70250af6bc
Christian Martorano <christiansmartorano@gmail.com>
2018-12-19 15:06:23 +0000

    Adjusting Ajax request and stock_controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,13 @@
 class StocksController < ApplicationController
-
   def search
-    @stock = Stock.search_stock(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].blank?
+      flash[:danger] = "You have entered empty value."
+    else
+      @stock = Stock.search_stock(params[:stock])
+      flash[:danger] = "You have entered an incorrect Symbol." unless @stock
+    end
+    respond_to do |format|
+      format.js {render partial: 'users/result'}
+    end
   end
-
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,13 @@
 class Stock < ApplicationRecord
 
   def self.search_stock(symbol)
-    stock = StockQuote::Stock.quote(symbol)
-    price = strip_commas(stock.latest_price)
-    self.new(ticker: stock.symbol, name: stock.company_name, last_price: price)
+    begin
+      stock = StockQuote::Stock.quote(symbol)
+      price = strip_commas(stock.latest_price)
+      self.new(ticker: stock.symbol, name: stock.company_name, last_price: price)
+    rescue Exception => e      
+      return nil
+    end
   end
 
   private
